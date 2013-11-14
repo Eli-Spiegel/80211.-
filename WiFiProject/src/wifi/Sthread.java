80b0abe9257d617cbe59a223e3c726d4fb89cfd6
espiegelups <eli s@Elis-MacBook-Pro-2.local>
2013-11-13 16:29:43 -0800

    update

--- WiFiProject/src/wifi/Sthread.java
+++ WiFiProject/src/wifi/Sthread.java
@@ -10,20 +10,20 @@ import java.util.concurrent.ArrayBlockingQueue;
 import rf.RF;
 
 /**
- * 
+ *
  * @author lswanson
  *
  */
 public class Sthread implements Runnable {
-
+    
 	private  byte[] packet;
 	private  RF theRF;
 	private ArrayBlockingQueue <byte []> ablockQf;
-
+    
 	
 	/**
 	 * Another constructor for the sending thread.
-	 * 
+	 *
 	 * @param packet data to be sent
 	 * @param dest destination address
 	 * @param theRF the RF layer we're using
@@ -34,41 +34,44 @@ public class Sthread implements Runnable {
 		ablockQf = abq;
 		this.theRF=theRF;
 	}
-
-
-
+    
+    
+    
 	/**
 	 * After an initial line of ouptut, the run() method here just loops forever,
-	 * printing periodically as it goes. 
+	 * printing periodically as it goes.
 	 */
 	public void run() {
 		System.out.println("send is alive and well");
 		while(true){
-			try {
-				
-				theRF.transmit(ablockQf.take());
-				System.out.print(" it should be sending for real");
-
+			if(theRF.inUse()){}
+			else{
+                try {
+                    
+                    theRF.transmit(ablockQf.take());
+                    System.out.print(" it should be sending for real");
+                    
 				}
-
-			 catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				System.out.println(e);
-				//e.printStackTrace();
-			
-		}
-
-	}
-
-	/*
-	public void sender(byte[] buf, short destAddr, short sourceAddr) {
-
-		packet=BuildPacket.build(buf, destAddr, sourceAddr);	
-		//ablockQ.put(packet);
-		//theRF.transmit(packet);
-	}
-	 */
-}
+                
+                catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    System.out.println(e);
+                    //e.printStackTrace();
+                    
+                }
+			}
+            
+        }
+        
+        /*
+         public void sender(byte[] buf, short destAddr, short sourceAddr) {
+         
+         packet=BuildPacket.build(buf, destAddr, sourceAddr);	
+         //ablockQ.put(packet);
+         //theRF.transmit(packet);
+         }
+         */
+    }
 }
 
 
